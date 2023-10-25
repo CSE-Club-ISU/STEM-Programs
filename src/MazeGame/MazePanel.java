bc7d78effc7336e770f504b0b19c31b951b12ddb
Oskar Niesen <oskar-github@niesens.com>
2023-10-25 16:07:15 -0500

    Added resizing to the maze

--- src/MazeGame/MazePanel.java
+++ src/MazeGame/MazePanel.java
@@ -9,21 +9,32 @@ public class MazePanel extends JPanel {
     MazeGame mazeGame;
     Panel mazePanel;
     CellUI[][] mazeUI;
+    JTextField sizeInput;
     public MazePanel() {
         BoxLayout boxLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
         setLayout(boxLayout);
         mazeUI = new CellUI[10][10];
         Frame.addTextToComp("Maze Game", this);
+        Box top = Box.createHorizontalBox();
+
         JButton resetButton = new JButton("Regenerate");
         resetButton.setVerticalTextPosition(AbstractButton.CENTER);
         resetButton.setAlignmentX(CENTER_ALIGNMENT);
-        resetButton.addActionListener((l) -> {mazeGame.startMazeGame(); refreshCells(); });
+        resetButton.addActionListener((l) -> regenerateMaze());
         resetButton.setBackground(Color.BLUE);
         resetButton.setForeground(Color.white);
         resetButton.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         resetButton.setFocusPainted(false);
-        this.add(resetButton);
+        top.add(resetButton);
 
+        sizeInput = new JTextField(Integer.toString(mazeUI.length));
+        sizeInput.setMaximumSize(new Dimension(100,30));
+        top.add(sizeInput);
+        add(top);
+        mazePanel = new Panel();
+        mazePanel.setBackground(Color.LIGHT_GRAY);
+        mazePanel.setMaximumSize(new Dimension(700,700));
+        this.add(mazePanel);
         generateMaze();
     }
 
@@ -35,16 +46,33 @@ public class MazePanel extends JPanel {
         }
     }
 
+    public void regenerateMaze() {
+        try {
+            int size = Integer.parseInt(sizeInput.getText());
+            if (size != mazeUI.length) {
+                for (int r = 0; r < mazeUI.length; r++) {
+                    for (int c = 0; c < mazeUI[r].length; c++) {
+                        mazePanel.remove(mazeUI[r][c]);
+                    }
+                }
+                this.paintAll(getGraphics());
+                mazeUI = new CellUI[size][size];
+                generateMaze();
+            } else {
+                mazeGame.startMazeGame();
+                refreshCells();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Input not valid");
+        }
+    }
+
     public void generateMaze() {
         mazeGame = new MazeGame(mazeUI.length, mazeUI[0].length);
         Cell[][] maze = mazeGame.getMaze();
-        mazePanel = new Panel();
-        mazePanel.setBackground(Color.LIGHT_GRAY);
-        mazePanel.setMaximumSize(new Dimension(700,700));
         GridLayout gridLayout = new GridLayout();
         gridLayout.setRows(maze.length);
         gridLayout.setColumns(maze[0].length);
-
         mazePanel.setLayout(gridLayout);
         for (int r = 0; r < maze.length; r++) {
             for (int c = 0; c < maze[r].length; c++) {
@@ -52,7 +80,6 @@ public class MazePanel extends JPanel {
                 mazePanel.add(mazeUI[r][c]);
             }
         }
-        this.add(mazePanel);
         paintAll(getGraphics());
     }
 }
