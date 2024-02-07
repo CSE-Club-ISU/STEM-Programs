83bb1b9049050621f5f5e047d78d86a337998e93
Oskar Niesen <oskar-github@niesens.com>
2024-02-06 19:55:57 -0600

    Added random maze generation again

--- src/MazeGame/MazeGame.java
+++ src/MazeGame/MazeGame.java
@@ -1,13 +1,16 @@
 package src.MazeGame;
 
 import src.MazeGame.MazeGeneration.DFS;
+import src.MazeGame.MazeGeneration.DFS2;
 import src.MazeGame.MazeGeneration.MazeGenAlgorithm;
+import src.MazeGame.MazeGeneration.Prims;
 import src.MazeGame.MazeGoalGeneneration.RandomGoalGen;
 import src.MazeGame.MazeGoalGeneneration.MazeGoalGenAlgorithm;
 import src.MazeGame.MazeStartGeneration.MazeStartGenAlgorithm;
 import src.MazeGame.MazeStartGeneration.RandomStartGen;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MazeGame {
     private Cell[][] grid;
@@ -17,10 +20,20 @@ public class MazeGame {
     String algorithmName;
     ArrayList<Integer> solutionInstructions;
 
+    /**
+     * Initializes the maze game with random generation
+     * @param rows the number of rows in the grid
+     * @param columns the number of columns in the grid
+     */
     public MazeGame(int rows, int columns) {
-        this(rows,columns, new RandomStartGen(), new DFS(), new RandomGoalGen());
+        this(rows,columns, new RandomStartGen(), getRandomMazeGenerationAlgorithm(), new RandomGoalGen());
     }
 
+    /**
+     * Initializes the maze game with the given generation parameters
+     * @param rows the number of rows in the grid
+     * @param columns the number of columns in the grid
+     */
     public MazeGame(int rows, int columns, MazeStartGenAlgorithm startGen, MazeGenAlgorithm mazeGen, MazeGoalGenAlgorithm goalGen) {
         pastVisitedValue = 0;
         grid = new Cell[rows][columns];
@@ -32,6 +45,17 @@ public class MazeGame {
         algorithmName = "None";
         solutionInstructions = new ArrayList<>();
         startMazeGame(startGen, mazeGen, goalGen);
+    }
+
+    private static MazeGenAlgorithm getRandomMazeGenerationAlgorithm() {
+        int algorithm = new Random().nextInt(5);
+        if (algorithm == 0) {
+            return new Prims();
+        } else if (algorithm <= 2) {
+            return new DFS();
+        } else {
+            return new DFS2();
+        }
     }
 
     public void startMazeGame(MazeStartGenAlgorithm startGen, MazeGenAlgorithm mazeGen, MazeGoalGenAlgorithm goalGen) {
